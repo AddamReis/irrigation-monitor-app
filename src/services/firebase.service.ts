a0a257c46aebfc8d89775612fa0f664458ff51a9
addam <addamreis@gmail.com>
2024-10-07 20:20:39 -0300

    adicionado metodo firebase

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Database, ref, set, get, child, update, remove } from '@angular/fire/database';
-import { getDatabase } from 'firebase/database';
+import { onValue } from 'firebase/database';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,27 @@ export class FirebaseService {
 
   constructor(private db: Database) { }
 
-  addData(path: string, data: any) {
-    const dbRef = ref(this.db, path);
-    return set(dbRef, data);
-  }
-
   getData(path: string) {
     const dbRef = ref(this.db);
     return get(child(dbRef, path));
+  }
+
+  getDataRealTime(path: string): Observable<any> {
+    const dbRef = ref(this.db, path);
+    return new Observable(observer => {
+      const unsubscribe = onValue(dbRef, (snapshot) => {
+        const data = snapshot.val();
+        observer.next(data);
+      }, {
+        onlyOnce: false
+      });
+      return () => unsubscribe();
+    });
+  }
+
+  addData(path: string, data: any) {
+    const dbRef = ref(this.db, path);
+    return set(dbRef, data);
   }
 
   updateData(path: string, data: any) {
