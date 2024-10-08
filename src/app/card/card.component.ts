baaebb5864f1b4e5d1ed1366fcbeaa1a5778ec60
addam <addamreis@gmail.com>
2024-10-08 18:49:54 -0300

    ajustes escala de cor do card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,8 +10,8 @@ export class CardComponent {
   @Input() icon!: string;
   @Input() title: string = 'Card Title';
   @Input() percentage!: number;
-  @Input() width: number = 220;
-  @Input() height: number = 220;
+  @Input() width: number = 240;
+  @Input() height: number = 240;
   @Output() onClick = new EventEmitter<void>();
 
   public color!: string;
@@ -29,20 +29,14 @@ export class CardComponent {
   }
 
   getColorByPercentage(percentage: number): string {
-    if (percentage >= 90) {
-      return '#4CAF50'; // Verde
-    } else if (percentage >= 80) {
+    if (percentage >= 75) {
       return '#7CB342'; // Verde médio
     } else if (percentage >= 70) {
-      return '#9E9D24'; // Verde amarelado
-    } else if (percentage >= 60) {
       return '#C0CA33'; // Amarelo esverdeado
     } else if (percentage >= 50) {
       return '#FBC02D'; // Amarelo
     } else if (percentage >= 40) {
       return '#FFA000'; // Laranja amarelado
-    } else if (percentage >= 30) {
-      return '#F57C00'; // Laranja
     } else if (percentage >= 20) {
       return '#E64A19'; // Laranja avermelhado
     } else if (percentage >= 10) {
