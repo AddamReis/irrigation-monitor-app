aba3dc5dfdad52d7b1a248417b939a3c56fbd29a
addam <addamreis@gmail.com>
2024-10-07 21:04:47 -0300

    implementação base da leitura e definição de interface

--- src/app/folder/pages/monitor/monitor.component.ts
+++ src/app/folder/pages/monitor/monitor.component.ts
@@ -11,14 +11,69 @@ export class MonitorComponent  implements OnInit {
   
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-
+  
+  public data: Sensor | null = null;
+  
   constructor(private firebaseService: FirebaseService) { }
 
   ngOnInit() {
+    const currentDate = new Date();
+
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
 
-    this.firebaseService.addData('users/user1', { name: 'Addam Reis', age: 28 })
+    /*this.firebaseService.addData('users/user1', { name: 'Addam Reis', age: 28 })
       .then(() => console.log('Dados adicionados com sucesso'))
-      .catch(err => console.error('Erro ao adicionar dados', err));
+      .catch(err => console.error('Erro ao adicionar dados', err));*/
+
+    //const fullDateTime = currentDate.toISOString().slice(0, 19).replace('T', ' ');
+
+    const year = currentDate.toISOString().slice(0, 4).replace('T', ' ');
+    const month = currentDate.toISOString().slice(5, 7);
+    const day = currentDate.toISOString().slice(8, 10);
+
+    this.firebaseService.getDataRealTime(`period/${year}/${month}/${day}`).subscribe((data) => {
+      const timeKeys = Object.keys(data);
+      timeKeys.sort((a, b) => { // Comparar as horas e minutos
+        const timeA = a.split('-').map(Number);
+        const timeB = b.split('-').map(Number);
+        return timeA[0] - timeB[0] || timeA[1] - timeB[1];
+      });
+      
+      const latestTimeKey = timeKeys[timeKeys.length - 1]; // A última chave é a mais recente
+      const latestData = data[latestTimeKey];
+
+      this.data = {
+        soilMoistureSensors: [],
+        soilMoisturePercentageMean: latestData.soilMoisturePercentageMean,
+        soilMoistureValueMean: latestData.soilMoistureValueMean,
+      };
+
+      this.addSensorData(latestData.soilMoisturePercentage1, latestData.soilMoistureValue1, 1);
+      this.addSensorData(latestData.soilMoisturePercentage2, latestData.soilMoistureValue2, 2);
+
+      console.log('Dados mais recentes:', this.data);
+    });
   }
+
+  private addSensorData(percentage: number | undefined, value: number | undefined, index: number) {
+    if (percentage !== undefined && value !== undefined) {
+      this.data?.soilMoistureSensors.push({
+        index: index,
+        soilMoisturePercentage: percentage,
+        soilMoistureValue: value,
+      });
+    }
+  }
+}
+
+export interface SoilMoistureSensor {
+  index: number;
+  soilMoisturePercentage: number;
+  soilMoistureValue: number;
+}
+
+export interface Sensor {
+  soilMoisturePercentageMean: number;
+  soilMoistureValueMean: number;
+  soilMoistureSensors: SoilMoistureSensor[];
 }
